eaeed0f50bf39981907ab1fa5e9ff52f075fc2bd
Cristian Oneț <cristian.onet@audionowdigital.com>
2017-09-06 13:21:50 +0300

    Improve the check service TLS deployment scripts.
    1. Reorganize and comment services definiton file.
    2. Improve name matching in the TLS check script (wildcards not supported)
    3. Better status messages
    4. Save the status when influxdb output format is selected

--- check-deployment/check-service.sh
+++ check-deployment/check-service.sh
@@ -8,32 +8,58 @@ format=$3
 
 if [ -z "$service_name" ] || [ -z "$service_port" ]
 then
-  echo "Usage: check-servcice.sh host port"
-  exit 1
+    echo "Usage: check-servcice.sh host port"
+    exit 1
 fi
 
 [ -z "$format" ] && format="h"
 
 echo -n | \
-openssl s_client -servername ${service_name} -connect ${service_name}:${service_port} 2>/dev/null | \
-openssl x509 -noout -dates -fingerprint -subject -nameopt nofname | \
-cut -d '=' -f 2 | \
-{
-  read from;
-  read to;
-  read sha1;
-  read subject;
-  subject="${subject##*,}"
-  from_seconds=`date --date="$from" "+%s"`
-  to_seconds=`date --date="$to" "+%s"`
-  now_seconds=`date "+%s"`
-  if [ "$format" = "h" ]
-  then
-      seconds="$(($to_seconds - $now_seconds))"
-      [ $seconds -gt 0 ] && status="valid" || status="invalid"
-      [ "${service_name}" != "$subject" ] && extra_status=" but subject ${subject} does not match name ${service_name}"
-      echo "${service_name}:${service_port} is $status for $(($seconds/86400)) days${extra_status}"
-  else
-      echo tls_verify,host=${service_name},port=${service_port} seconds_from_valid=$(($now_seconds - $from_seconds)),seconds_to_expire=$(($to_seconds - $now_seconds)),sha1=\"$sha1\"
-  fi
-}
+    openssl s_client -servername ${service_name} -connect ${service_name}:${service_port} 2>/dev/null | \
+    openssl x509 -noout -text -certopt no_header,no_version,no_serial,no_signame,no_issuer,no_pubkey,no_sigdump,no_aux 2>/dev/null | \
+    {
+	while read line
+	do
+	    case $line in
+		Not\ Before* )
+		    from="${line#*:}"
+		    ;;
+		Not\ After* )
+		    to="${line#*:}"
+		    ;;
+		Subject* )
+		    subject="${line#*CN=}"
+		    ;;
+		X509v3\ Subject\ Alternative\ Name* )
+		    read alternative_names
+		    alternative_names=$(echo ${alternative_names} | tr ", DNS:" " " | tr -s " ")
+		    ;;
+	    esac
+	done
+	status=0
+	# service is probably down
+	[ -z "$from" ] || [ -z "$to" ] || [ -z "$subject" ] && { status=1; status_message="invalid, failed to connect/obtain certificate information"; }
+	from_seconds=`date --date="$from" "+%s"`
+	to_seconds=`date --date="$to" "+%s"`
+	now_seconds=`date "+%s"`
+	seconds="$(($to_seconds - $now_seconds))"
+	if [ $status -eq 0 ]
+	then
+	    [ $seconds -le 0 ] && { status=2; status_message="invalid, certificate expired $((-$seconds/86400)) days ago"; }
+	    name_match=0
+	    for alternative_name in $alternative_names
+	    do
+		# note that this does not support wildcard matching 
+		[ "${service_name}" = "${alternative_name}" ] && name_match=1 && break
+	    done
+	    [ $name_match -eq 0 ] && { status=3; status_message="invalid, name ${service_name} does not match alternative names${alternative_names}"; }
+	fi
+	if [ "$format" = "h" ]
+	then
+	    [ $status -eq 0 ] && status_message="valid, for $(($seconds/86400)) days"
+	    echo "${service_name}:${service_port} is ${status_message}"
+	else
+	    # status mapping: 0 - service OK, 1 - service down, 2 - service up but expired certificate, 3 - service up,certificate valid, but name does not match alternative names
+	    echo tls_verify,host=${service_name},port=${service_port} seconds_from_valid=$(($now_seconds - $from_seconds)),seconds_to_expire=$(($to_seconds - $now_seconds)),status=$status
+	fi
+    }
